3757af92985726d478b724a736aa4e9ef4223237
binyamin.greenberg <binyamin.greenberg@servicenow.com>
2017-07-21 15:25:01 +0300

    make config minimizable

--- app/app.module.js
+++ app/app.module.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['ngMaterial', 'ngCookies']);
-app.config(function ($locationProvider) {
+app.config(['$locationProvider',function ($locationProvider) {
     $locationProvider.html5Mode(true);
-});
+}]);
 require('./controller.appCtr.js');
 require('./directive.chipsInput.js');
 require('./directive.addPost');
